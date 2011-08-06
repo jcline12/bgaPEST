1e1ec4cf6e59717aef46ecef533360a50f6d33ae
Mike Fienen <mnfienen@usgs.gov>
2011-08-05 21:27:04 -0500

    More progress on forming struct par ob fun. See notes within code

--- bgaPEST/src/bso_structural_optimization.f90
+++ bgaPEST/src/bso_structural_optimization.f90
@@ -41,10 +41,22 @@ real (kind = 8) function SP_min(theta, sig, d_XQR, Q0_all,cv_OBS, d_OBS, cv_A, d
    double precision,     intent(in)     :: sig
    double precision                     :: z(cv_OBS%nobs) 
    double precision                     :: HXB(cv_OBS%nobs)
+   double precision                     :: HXQbb(cv_OBS%nobs,cv_PAR%p)
+   double precision                     :: OMEGA(cv_OBS%nobs,cv_OBS%nobs)
+   double precision                     :: Gyy(cv_OBS%nobs,cv_OBS%nobs)
+
+   !------------------------@@@@@@@@@@@@@@@@@@@@----------------------@@@@@@@@@@@@@@@@@@@@@@--------------
+   !
+   !
+   !    VITAL ISSUE HERE! -- We need to bring in theta and sig as a single vector but also parse it accordingly
+   !
+   !
+   !------------------------@@@@@@@@@@@@@@@@@@@@----------------------@@@@@@@@@@@@@@@@@@@@@@--------------
+   
 
 
    !----------------------------- Form the linearization-corrected residuals --------------------------
-   HXB = UNINIT_REAL ! -- matrix (nobs x p)
+   HXB = UNINIT_REAL ! -- matrix (nobs)
    call DGEMV('n',cv_OBS%nobs, cv_PAR%p, 1.D0, d_A%HX, cv_OBS%nobs, &
             d_PM%beta_0, 1, 0.D0, HXB,1)
               
@@ -53,6 +65,30 @@ real (kind = 8) function SP_min(theta, sig, d_XQR, Q0_all,cv_OBS, d_OBS, cv_A, d
    !----------------------------- Form Gyy with the current values of theta and sigma------------------
    call bmo_form_Qss_Qsy(d_XQR, theta, cv_PAR, cv_OBS, cv_S, cv_A, d_A, d_PAR, Q0_All)
    call bmo_form_HQsy_Qyy(d_XQR, sig, cv_PAR, cv_OBS, d_A)
+
+   HXQbb = UNINIT_REAL ! -- matrix (nobs x p)
+   
+   ! -- form HXQbb
+   call dgemm('n', 'n', cv_OBS%nobs, cv_PAR%p,  cv_PAR%p, 1.D0, d_A%HX, &
+              cv_OBS%nobs, d_PM%Qbb, cv_PAR%p, 0.D0, HXQbb, cv_OBS%nobs)
+   
+   ! -- now multiply HXQbb x (HX)' to form OMEGA
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! MNF --- need to sort out all these dimensions
+   call dgemm('n', 't', cv_OBS%nobs, cv_OBS%nobs,  cv_PAR%p, 1.D0, HXQbb, &
+              cv_OBS%nobs, d_A%HX, cv_PAR%p, 0.D0, OMEGA, cv_OBS%nobs)
+! MNF fix the above
+!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+   ! -- add OMEGA + Qyy to form Gyy
+   Gyy = OMEGA + d_A%Qyy
+
+   !----------------------------- Calculate the determinant term --------------------------------------
+   
+   !-- First perform LU decomposition on Gyy
+
+   ! -- WHEN WE GET TO FORMING z*'Gyy(^-1)*z WE SHOULD CREATE A PSEUDO LINEAR SYSTEM FOR Gyy^(-1)*z
 
 SP_min = 0.0 !theta objective function here!
 return
