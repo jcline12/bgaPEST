a972d41334e2363ab15308bd0a85e704e5301519
Michael Fienen <mnfienen@usgs.gov>
2012-09-06 16:45:53 -0500

    end of day commit. Almost have Jacobian filler finished. Just need to: 1) Figure out why one fewer model run than there should be, and 2) need to make the derivatives calc (currently fillin JAC with observations only)

--- bgaPEST_VS10/src/python/Condor_ATC.py
+++ bgaPEST_VS10/src/python/Condor_ATC.py
@@ -1,10 +1,51 @@
 import numpy as np
-
+import subprocess as sub
 import parallel_condor_Jacobian as pcj
+import os, shutil
+
+
+
+
 
 # initialize a Jacobian Master object to hold results
 fulljack = pcj.Jacobian_Master()
 
+fulljack.read_obs_names()
 
+fulljack.read_pars()
 
-i=1
+fulljack.read_mio_ins()
+
+fulljack.jacfolder = '#jacfilestmp#'
+
+'''
+# make a scratch directory for all the output files
+fulljack.jacfolder = '#jacfilestmp#'
+if os.path.exists(os.path.join(os.getcwd(),fulljack.jacfolder)):
+    shutil.rmtree(os.path.join(os.getcwd(),fulljack.jacfolder))
+os.mkdir(os.path.join(os.getcwd(),fulljack.jacfolder))
+
+# run all the runs --> without CONDOR FOR NOW
+for i in np.arange(fulljack.NPAR):
+    print 'running run number ---> %d' %(i)
+    sub.call('python condor_single_run.py %d' %(i))
+    
+# finally run the base case
+sub.call('python condor_single_run.py -999')
+
+# this is only in place until redirect is made (using python)
+for cf in os.listdir(os.getcwd()):
+    if '.obf.' in cf:
+        shutil.move(cf,os.path.join(os.getcwd(),fulljack.jacfolder,cf))
+'''
+
+# read in the results and populate the Jacobian
+fulljack.JAC = np.zeros((fulljack.NOBS,fulljack.NPAR))
+
+fulljack.read_obs_files(-999)
+
+for i in np.arange(fulljack.NPAR):
+    fulljack.read_obs_files(i)
+    
+
+k=1
--- bgaPEST_VS10/src/python/condor_single_run.py
+++ bgaPEST_VS10/src/python/condor_single_run.py
@@ -10,12 +10,23 @@ parind = int(sys.argv[1]) #index for which parameter to perturb
 # ####### #
  # M A I N #
   # ####### #
+# initialize a single model run object
 jack_one_run = pcj.Jacobian_one_run()
 
+# determine which parameter index to perturb
 jack_one_run.perturb = int(sys.argv[1])
 
+# read in the parameter values and meta data for the run
 jack_one_run.read_parameters_and_meta_data()
 
+# create the model input files using TEMPCHEK
 jack_one_run.make_model_input()
+
+# run the model by calling the command line argument passed from bgaPEST
+jack_one_run.run_model()
+
+# get the observation values and save to proper filename
+jack_one_run.read_obs()
+
 
 i=1
