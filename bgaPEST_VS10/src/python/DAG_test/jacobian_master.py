1ce984edebde2314e69fece67b3d6f13649aad43
Michael Fienen <mnfienen@usgs.gov>
2012-09-11 17:01:24 -0500

    fixed notification is never for condor_submit_dag bug

--- bgaPEST_VS10/src/python/DAG_test/jacobian_master.py
+++ bgaPEST_VS10/src/python/DAG_test/jacobian_master.py
@@ -25,7 +25,7 @@ for cf in os.listdir(os.getcwd()):
 	if dag_sub + '.' in cf:
 		os.remove(os.path.join(os.getcwd(),cf))
 		print 'removing --> ' + os.path.join(os.getcwd(),cf)
-sub.call('condor_submit_dag ' + dag_sub + ' -notification=never ', shell=True)
+sub.call('condor_submit_dag  -notification never ' + dag_sub, shell=True)
 while jac_in_proc:
 	if os.path.exists(os.path.join(os.getcwd(),'jacdone.#stat')):
 		jac_in_proc = False
