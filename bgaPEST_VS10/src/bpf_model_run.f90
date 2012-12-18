04117dd057f60fd0d51620601655880a290645e6
Marco D'Oria <marco.doria@unipr.it>
2012-12-18 14:10:38 +0100

    1. Removed a unnecessary forward run at each iteration (using external derivatives, PEST derivatives or Condor stuff). Substituted with a forward run upfront (at the very beginning of the main).
    2. Added variable derinc into the input files (.bgp) of the reverse
    routing examlpes.

--- bgaPEST_VS10/src/bpf_model_run.f90
+++ bgaPEST_VS10/src/bpf_model_run.f90
@@ -61,22 +61,24 @@ contains
               call utl_bomb_out(errstruc)
             endif 
         case (1) ! external PEST-style Jacobian
-            call system(d_MOD%com) ! run the model once, forward, to have outputs with current parameters 
-            !-- MIO read the ouput file results and update 
-            if(mio_read_model_output_files(errstruc,miostruc, d_OBS%h).ne.0) then
-              call utl_bomb_out(errstruc) 
-            endif 
+         !MD Done at the very beginning !
+         !MD!   call system(d_MOD%com) ! run the model once, forward, to have outputs with current parameters 
+         !MD!   !-- MIO read the ouput file results and update 
+         !MD!   if(mio_read_model_output_files(errstruc,miostruc, d_OBS%h).ne.0) then
+         !MD!     call utl_bomb_out(errstruc) 
+         !MD!   endif 
             !-- create PEST input files and run PEST          
             call bxd_write_param_file(cv_PAR,d_PAR) ! write the parameter file
             call system('pst_generator.exe')        ! create the necessary PEST control file
             call system('run_pest_scratch.bat')     ! run PEST externally for derivatives
             call readJCO('scratch.jco', d_A)
         case (2) ! dercom alternative Jacobian
-            call system(d_MOD%com) !Rum forward model once to obtain the current outputs, before running external derivative model
-            !-- MIO read the ouput file results and update 
-            if(mio_read_model_output_files(errstruc,miostruc, d_OBS%h).ne.0) then
-              call utl_bomb_out(errstruc)
-            endif
+         !MD Done at the very beginning !
+         !MD!   call system(d_MOD%com) !Rum forward model once to obtain the current outputs, before running external derivative model
+         !MD!   !-- MIO read the ouput file results and update 
+         !MD!   if(mio_read_model_output_files(errstruc,miostruc, d_OBS%h).ne.0) then
+         !MD!     call utl_bomb_out(errstruc)
+         !MD!   endif
             
             call system(d_MOD%dercom) !Run the external derivative model 
             select case (cv_A%jacobian_format)
@@ -93,11 +95,12 @@ contains
               call utl_bomb_out(errstruc)        
             endif 
         case (4) ! Parallel Jacobian Using Condor Externally
-            call system(d_MOD%com) ! run the model once, forward, to have outputs with current parameters 
-            !-- MIO read the ouput file results and update 
-            if(mio_read_model_output_files(errstruc,miostruc, d_OBS%h).ne.0) then
-              call utl_bomb_out(errstruc) 
-            endif
+         !MD Done at the very beginning !
+         !MD!   call system(d_MOD%com) ! run the model once, forward, to have outputs with current parameters 
+         !MD!   !-- MIO read the ouput file results and update 
+         !MD!   if(mio_read_model_output_files(errstruc,miostruc, d_OBS%h).ne.0) then
+         !MD!     call utl_bomb_out(errstruc) 
+         !MD!   endif
             call bxd_write_param_file(cv_PAR,d_PAR) ! write the parameter file
             call system('python CondorATC.py')
             select case (cv_A%jacobian_format)
